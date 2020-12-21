26e128c21cf141b91cc2a6a6595a4613879e9e13
solismc <manolocsolis@gmail.com>
2020-12-21 09:36:10 -0500

    Added controlling loops examples

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,21 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 console.log('Hello World!!!!!!');
+
+const carId= 10;
+console.log(carId);
+
+/*let i=0;
+for (; i<12; i++) {
+  if (i === 8) {
+    break;
+  }
+}
+
+console.log(i);*/
+
+for (let i=0; i<5; i++) {
+  if (i === 3) 
+    continue;
+    console.log(i);
+  }
